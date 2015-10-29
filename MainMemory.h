f1742fc2166a105ab56ea520957c94de11b041a7
JeanRusli <jean_rusli@yahoo.com>
2015-10-29 00:02:04 -0500

    Update MainMemory.h

--- MainMemory.h
+++ MainMemory.h
@@ -8,5 +8,13 @@
 
 #include "MainMemory.c"
 
+#define MainMemSize 1024;
+
+public:
+  void MemoryRead(uint32_t* addr, uint32_t* RdData);
+  void MemoryWrite(uint32_t* addr, uint32_t* WrtData);
+  
+private:
+  uint32_t MainMem[MainMemSize];
 
 #endif
