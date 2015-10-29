cd3b72ece7d18406845091ffb03980e00bc2f4de
JeanRusli <jean_rusli@yahoo.com>
2015-10-29 00:34:08 -0500

    Update MainMemory.h

--- MainMemory.h
+++ MainMemory.h
@@ -1,6 +1,6 @@
 
-#ifndef MAINMEMORY_H
-#define MAINMEMORY_H
+#ifndef __MAINMEMORY_H
+#define __MAINMEMORY_H
 
 #include "stdbool.h"
 #include "stdint.h"
@@ -10,11 +10,18 @@
 
 #define MainMemSize 1024;
 
+// Define the structure of Main Memory.
+// The members include the address and data.
+struct MainMem {
+  uint32_t addr;
+  uint32_t data;
+};
+
 public:
   void MemoryRead(uint32_t* addr, uint32_t* RdData);
   void MemoryWrite(uint32_t* addr, uint32_t* WrtData);
   
 private:
-  uint32_t MainMem[MainMemSize];
+  struct MainMem Mem[MainMemSize];
 
 #endif
