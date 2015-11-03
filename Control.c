051caa3641388e634460efe361fe284b7a4219ba
JeanRusli <jean_rusli@yahoo.com>
2015-11-02 20:11:33 -0600

    Update Control.c

--- Control.c
+++ Control.c
@@ -1,1 +1,20 @@
-
+void ControlInfo(uint32_t contArr[], uint32_t funct) {
+  
+  // Switch statement to determine which action to take based on the value
+  // of funct.
+  switch(funct) {
+  case 32: // Bitwise AND
+    contArr[0] = 
+    contArr[0] = 
+    contArr[0] = 
+    contArr[0] = 
+    contArr[0] = 
+    contArr[0] = 
+    contArr[0] = 
+    break;
+  default :
+    printf("Invalid function.\n" );
+    break;
+  }  
+  
+}
