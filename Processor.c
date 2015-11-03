2a300f26f4d1b217a45d175b23a2d7ade684b45f
JeanRusli <jean_rusli@yahoo.com>
2015-11-02 21:30:26 -0600

    Update Processor.c

--- Processor.c
+++ Processor.c
@@ -1,1 +1,47 @@
 
+Processor() {
+  PC = 0;
+  int i;
+	for(i = 0; i < 9; i++) {
+		conArr[i] = 0;
+	}
+}
+
+void execProcessor(uint32_t instr) {
+	PC = PC + 4;
+	uint32_t type = instr >> 26;
+	switch(type) {
+  	case 0: // R type
+    	executeRType(instr);
+    	break;
+  	default :
+	    printf("Invalid instruction.\n" );
+	    break;
+	}
+}
+
+void executeRType(uint32_t instr) {
+	uint32_t rs = (instr << 6) >> 27;
+	uint32_t rt = (instr << 11) >> 27;
+	uint32_t rd = (instr << 16) >> 27;
+	uint32_t shamt = (instr << 21) >> 27;
+	uint32_t funct = (instr << 26) >> 26;
+	
+	uint32_t data1 = readData(rs);
+	uint32_t data2 = readData(rt);
+	
+	ControlInfo(conArr,funct);
+	uint32_t result = ALU(conArr[5],data1,data2);
+	if(conArr[8] == 1) {
+		writeData(rd,result);
+	}
+}
+
+void executeIType(uint32_t instr) {
+	
+}
+
+void executeJType(uint32_t instr) {
+	
+
+}
