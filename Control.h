005b348ea56d4dbf6048960dfa94fa006e98661d
JeanRusli <jean_rusli@yahoo.com>
2015-11-02 20:12:18 -0600

    Update Control.h

--- Control.h
+++ Control.h
@@ -1,1 +1,13 @@
+#ifndef CONTROL_H
+#define CONTROL_H
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+#include "Control.c"
+
+public:
+    void ControlInfo(uint32_t contArr[], uint32_t funct);
+
+#endif
