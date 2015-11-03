2225448f40d0f758a34468132c827e27487ea818
JeanRusli <jean_rusli@yahoo.com>
2015-11-02 23:06:40 -0600

    Update RegFile.h

--- RegFile.h
+++ RegFile.h
@@ -10,12 +10,14 @@
 //Defines the Register Size
 #define uint32_t RegMemSize 32;
 
-public:
-	RegFile();
-	uint32_t readData(uint32_t addr);
-	void writeData(uint32_t addr, uint32_t data);
-
-private:
-	uint32_t RegFileArr; //[RegMemSize]
+class RegFile {
+	public:
+		RegFile();
+		uint32_t readData(uint32_t addr);
+		void writeData(uint32_t addr, uint32_t data);
+	
+	private:
+		uint32_t RegFileArr; //[RegMemSize]
+};
 
 #endif
