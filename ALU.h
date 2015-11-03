6ed809e1e46a91e385277be572d4b1b57b01dcf7
JeanRusli <jean_rusli@yahoo.com>
2015-11-03 00:20:08 -0600

    Update ALU.h

--- ALU.h
+++ ALU.h
@@ -7,9 +7,7 @@
 #include <stdint.h>
 #include <stdarg.h>
     
-typedef struct ALU {
-    bool ALU_Zero;
-} *ALU_T;
+typedef struct ALU *ALU_T;
 
 extern ALU_T ALU_new(void);
 extern uint32_t ALU(uint32_t ALUop, uint32_t SData, uint32_t TData);
