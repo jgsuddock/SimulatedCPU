272c652fed6fa14eb981042ae6cfae10d74e5d0d
JeanRusli <jean_rusli@yahoo.com>
2015-11-03 00:19:41 -0600

    Update RegFile.h

--- RegFile.h
+++ RegFile.h
@@ -11,9 +11,7 @@
 //Defines the Register Size
 //#define uint32_t RegMemSize 32;
 
-typedef struct RegFile {
-	uint32_t RegFileArr[32];
-} *RegFile_T;
+typedef struct RegFile *RegFile_T;
 
 extern RegFile_T RegFile_new(void);
 extern uint32_t readData(uint32_t addr);
