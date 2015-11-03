dcfdd8457bc7038198a33a1ee9d2d4921921bee6
JeanRusli <jean_rusli@yahoo.com>
2015-11-03 03:36:11 -0600

    Update Control.h

--- Control.h
+++ Control.h
@@ -7,8 +7,8 @@
  * Description: This project simulates the components of a MIPS
  * 				CPU and how they interact.
  * 
- * Created by: Jake Suddock
- * 			Jeanette Rusli
+ * Created by:  Jake Suddock
+ * 			        Jeanette Rusli
  * 
  * Last Updated: November 3, 2015 @ 2:00
  * 
