7c4ff2b827e1a2409b3dd425b24ffabe0ae2b82b
JeanRusli <jean_rusli@yahoo.com>
2015-11-03 00:47:44 -0600

    Update Processor.c

--- Processor.c
+++ Processor.c
@@ -35,12 +35,12 @@ Processor_T Processor_new(void) {
 	return P;
 }
 
-void execProcessor(Processor_T P, uint32_t instr) {
+void execProcessor(Processor_T P, RegFile_T RF, uint32_t instr) {
 	P->PC++;
 	uint32_t type = instr >> 26;
 	switch(type) {
   	case 0: // R type
-    	executeRType(instr);
+    	executeRType(P, RF, instr);
     	break;
   	default :
 	    printf("Invalid instruction.\n" );
@@ -48,28 +48,28 @@ void execProcessor(Processor_T P, uint32_t instr) {
 	}
 }
 
-void executeRType(Processor_T P, uint32_t instr) {
+void executeRType(Processor_T P, RegFile_T RF, uint32_t instr) {
 	uint32_t rs = (instr << 6) >> 27;
 	uint32_t rt = (instr << 11) >> 27;
 	uint32_t rd = (instr << 16) >> 27;
 	uint32_t shamt = (instr << 21) >> 27;
 	uint32_t funct = (instr << 26) >> 26;
 	
-	uint32_t data1 = readData(rs);
-	uint32_t data2 = readData(rt);
+	uint32_t data1 = readData(RF,rs);
+	uint32_t data2 = readData(RF,rt);
 	
 	ControlInfo(P->conArr,funct);
 	uint32_t result = ALU(P->conArr[5],data1,data2);
 	if(P->conArr[8] == 1) {
-		writeData(rd,result);
+		writeData(RF,rd,result);
 	}
 }
 
-void executeIType(Processor_T P, uint32_t instr) {
+void executeIType(Processor_T P, RegFile_T RF, uint32_t instr) {
 	
 }
 
-void executeJType(Processor_T P, uint32_t instr) {
+void executeJType(Processor_T P, RegFile_T RF, uint32_t instr) {
 	
 
 }
