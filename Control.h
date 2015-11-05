6d344402c22a25fa00339830f2ba9cfa275b9f2e
Jake Suddock <jacob.suddock@gmail.com>
2015-11-05 08:19:20 -0600

    Update Control.h

--- Control.h
+++ Control.h
@@ -23,6 +23,10 @@
 #include <stdint.h>
 #include <stdarg.h>
 
+// Method to get all the control information.
+// The input includes the array that is used to store all the information and
+// the value of function. The value of function is the one that determines
+// the control information values.
 void ControlInfo(uint32_t contArr[], uint32_t funct);
 
 #endif
