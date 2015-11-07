318a2e3ae72cd80db48c5d7718364859f3b37e88
JeanRusli <jean_rusli@yahoo.com>
2015-11-07 14:34:35 -0600

    Update MainMemory.c

--- MainMemory.c
+++ MainMemory.c
@@ -13,35 +13,58 @@
  * Last Updated: November 3, 2015 @ 2:00
  * 
  */
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+#include "MainMemory.h"
+
+#define MainMemSize 1024
+
+struct MainMem {
+	uint32_t MainMemArr[MainMemSize];
+}; 
+
+MainMem_T MainMem_new(void) {
+	// Allocates memory for Register Array
+	MainMem_T M = malloc(sizeof(struct MainMem));
+	
+	// Initializes Register Array with values
+	int i;
+	for(i = 0; i < MainMemSize; i++) {
+		M->MainMemArr[i] = 0;
+	}
+	
+	return M;
+}
 
 // Method to read into the memory.
 // It will update the value of RdData.
-void MemoryRead(uint32_t* addr, uint32_t* RdData) {
+uint32_t MemoryRead(MainMem_T M, uint32_t addr) {
   
-  int i;
-  
-  // Loop to find the element in Main Memory that has the address.
-  for(i = 0; i < 1024; i++) {
-    if(Mem[i].addr == addr) {
-      // Update the value of RdData with the value in Main Memory.
-      *RdData = Mem[i].data; 
-    }
-  }
+  // Checks validity of input address
+	if(addr > MainMemSize){
+		printf("Error: Read addressing out of bounds!\n" );
+	} else {
+		// Returns data at address of register array
+		return M->MainMemArr[addr];
+	}
   
 }
 
 // Method to write into the memory.
 // It will write the value of WrtData in Main Memory.
-void MemoryWrite(uint32_t* addr, uint32_t* WrtData) {
+void MemoryWrite(MainMem_T M, uint32_t addr, uint32_t WrtData) {
   
-  int i;
-  
-  // Loop to find the element in Main Memory that has the address.
-  for(i = 0; i < 1024; i++) {
-    if(Mem[i].addr == addr) {
-      // Write the value of WrtData into Main Memory.
-      Mem[i].data = *WrtData;
-    }
-  }
+ 	// Checks validity of input address
+	if(addr > MainMemSize){
+		printf("Error: Write addressing out of bounds!\n" );
+	} else {
+		//Writes data to address of register file
+		M->MainMemArr[addr] = WrtData;
+	}
   
 }
