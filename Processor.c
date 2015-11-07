6ab9c1b33e842cd284599cc7dbb96ae7c3b64a53
JeanRusli <jean_rusli@yahoo.com>
2015-11-07 15:14:57 -0600

    Update Processor.c

--- Processor.c
+++ Processor.c
@@ -63,11 +63,11 @@ void execProcessor(Processor_T P, RegFile_T RF, uint32_t instr) {
 	// There are 3 types: R, I, and J.
 	switch(type) {
   	case 0: // R type
-    	executeRType(P, RF, instr); // Execute the instruction based on the type.
-    	break;
+    		executeRType(P, RF, instr); // Execute the instruction based on the type.
+    		break;
   	default :
-	    printf("Invalid instruction.\n" );
-	    break;
+		executeIType(P, RF, instr, type);
+	    	break;
 	}
 }
 
@@ -96,8 +96,51 @@ void executeRType(Processor_T P, RegFile_T RF, uint32_t instr) {
 	}
 }
 
-void executeIType(Processor_T P, RegFile_T RF, uint32_t instr) {
+void executeIType(Processor_T P, RegFile_T RF, MainMem_T M, uint32_t instr, uint32_t op) {
+	// Get all the parts of instructions using shift operation.
+	uint32_t rs = (instr << 6) >> 27;	// Get the source address.
+	uint32_t rt = (instr << 11) >> 27;	// Get the target address.
+	uint32_t imm = (instr << 16) >> 16;	// Get the value of function.
 	
+	// Get the control information based on the value of function.
+	ControlInfo(P->conArr,op);
+	
+	// Read the data in source and target address from Register File.
+	uint32_t data1 = readData(RF,rs);
+	uint32_t data2;
+	uint32_t result;
+	uint32_t RdData;
+	uint32_t WrtData;
+	
+	if(P->conArr[0] == -1) { // RegDst
+		data2 = readData(RF,rt);
+	} 
+	
+	if(P->conArr[7] == 0) { // ALUSrc
+		result = ALU(P->conArr[5],data1,data2);
+	} else {
+		result = ALU(P->conArr[5],data1,imm);
+	}
+	
+	if(P->conArr[6] == 1) { // MemWrite
+		MemoryWrite(M,result,data2);
+	} 
+	
+	if(P->conArr[3] == 1) { // MemRead
+		RdData = MemoryRead(M,result);
+	}
+
+	if(P->conArr[4] == 0) { // MemToReg
+		WrtData = result;
+	} else {
+		WrtData = RdData;
+	}
+	
+	// Based on the control information (RegWrite), determine if the result needs to be 
+	// written back in the register file.
+	if(P->conArr[8] == 1) {
+		writeData(RF,rt,WrtData); // Write result in destination address.
+	}
 }
 
 void executeJType(Processor_T P, RegFile_T RF, uint32_t instr) {
